23e74a6458c09994882d8858d11e548fb86ef4f8
Rachita38 <chauhan.rachita30@gmail.com>
2020-07-11 12:26:02 -0700

    Adding a Reusable component property-card

--- src/app/property-card/property-card.component.ts
+++ src/app/property-card/property-card.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
+
 
 @Component({
   selector: 'app-property-card',
@@ -7,6 +8,7 @@ import { Component, OnInit } from '@angular/core';
 })
 export class PropertyCardComponent implements OnInit {
 
+  @Input('Listing') listing: any;
   constructor() { }
 
   ngOnInit(): void {
